6bb811e14d7565b48b06d3edbfb3644f198ba093
sammytsukino <samy.cabello.g@gmail.com>
2025-02-21 09:56:18 +0100

    tetris

--- src/tetris/Board.java
+++ src/tetris/Board.java
@@ -1,5 +1,56 @@
 package tetris;
 
+import java.awt.Color;
+import java.awt.Point;
+
 public class Board {
+
+	private final int HEIGHT = 20;
+	private final int WIDTH = 10;
+	private Color[][] grid;
+
+	public Board() {
+		grid = new Color[HEIGHT][WIDTH];
+	}
+
+	public boolean canMove(Tetromino tetromino, int dx, int dy) {
+		for (Point p : tetromino.getShape()) {
+			int newX = p.x + dx;
+			int newY = p.y + dy;
+
+			if (newX < 0 || newX >= WIDTH || newY >= HEIGHT || (newY >= 0 && grid[newY][newX] != null)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void placeTetromino(Tetromino tetromino) {
+		for (Point p : tetromino.getShape()) {
+			grid[p.y][p.x] = tetromino.getColor();
+		}
+	}
+
+	public int clearLines() {
+		int linesCleared = 0;
+		for (int y = 0; y < HEIGHT; y++) {
+			boolean full = true;
+			for (int x = 0; x < WIDTH; x++) {
+				if (grid[y][x] == null) {
+					full = false;
+					break;
+				}
+			}
+			if (full) {
+				linesCleared++;
+				for (int shiftY = y; shiftY > 0; shiftY--) {
+					grid[shiftY] = grid[shiftY -1].clone();
+				}
+				grid[0] = new Color[WIDTH];
+			}
+		}
+		return linesCleared;
+
+	}
 
 }
--- src/tetris/Tetromino.java
+++ src/tetris/Tetromino.java
@@ -1,5 +1,34 @@
 package tetris;
 
-public class Tetromino {
+import java.awt.Color;
+import java.awt.Point;
 
+
+public class Tetromino {
+	private Point[] shape;
+	private Color color;
+	
+	public Tetromino(Point[] shape, Color color) {
+		super();
+		this.shape = shape;
+		this.color = color;
+	}
+
+	public Point [] getShape () {
+		return shape;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	public void rotate() {
+		for (Point p : shape) {
+			int temp = p.x;
+			p.x = -p.x;
+			p.y = temp;
+		}
+	}
+	
+	
 }
